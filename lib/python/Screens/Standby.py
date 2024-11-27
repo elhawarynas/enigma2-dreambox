60ad1346a27915d30089791a85cf4299390649a7
ims <ims21@users.sourceforge.net>
2024-11-27 17:08:19 +0100

    "[HDMI-CEC]"
    * added option for IP test before Standby or Deep stanby
    
    - when this option is enabled and the corresponding PC responds to the ping on the network,
     during the transition of the box to standby/deep standby, a 'sourceinactive' is sent instead of 'standby',
     preventing the TV from turning off.
    - By default, this option is disabled.
    
    * added option for detect PC to plugin

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -346,9 +346,10 @@ class TryQuitMainloop(MessageBox):
 				if not inStandby:
 					if os.path.exists("/usr/script/standby_enter.sh"):
 						Console().ePopen("/usr/script/standby_enter.sh")
-					if BoxInfo.getItem("HasHDMI-CEC") and config.hdmicec.enabled.value and config.hdmicec.control_tv_standby.value and config.hdmicec.next_boxes_detect.value:
-						import Components.HdmiCec
-						Components.HdmiCec.hdmi_cec.secondBoxActive()
+					if BoxInfo.getItem("HasHDMI-CEC") and config.hdmicec.enabled.value and ((config.hdmicec.control_tv_standby.value and (config.hdmicec.next_boxes_detect.value or config.hdmicec.ethernet_pc_used.value)) or config.hdmicec.handle_deepstandby_events.value != "no"):
+						if config.hdmicec.control_tv_standby.value and config.hdmicec.next_boxes_detect.value:
+							import Components.HdmiCec
+							Components.HdmiCec.hdmi_cec.secondBoxActive()
 						if not hasattr(self, "quitScreen"):
 							self.quitScreen = self.session.instantiateDialog(QuitMainloopScreen)
 							self.quitScreen.show()
